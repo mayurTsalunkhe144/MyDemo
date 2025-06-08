dbecabf6547169b3a75ba8db51939112083f0208
Mayur <mayurtsalunkhe@gmail.com>
2025-06-08 21:24:16 +0530

    Add new component

---
+++ src/helo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Helo() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React component.</p>
+    </div>
+  );
+}
+export default Helo;
